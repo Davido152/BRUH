4421d8ad8c4654cc0ff5a6b016e3d98c0bd907da
Davido152 <74314163+Davido152@users.noreply.github.com>
2020-12-30 11:27:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(client)
+    console.log("Servers:")
+    client.guilds.cache.forEach((guild) => {
+        console.log(' - ' + guild.name + ':')
+        guild.channels.cache.forEach((channel) => {
+            console.log(`   • ${channel.name} (${channel.type}) - ${channel.id}`)
+        })
+    })
+})
+client.on('message', (receivedMessage) => {
+    // Prevent bot from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+    else if (receivedMessage.content.includes('bruh')) {
+         receivedMessage.channel.send("**BRUH**")
+    }
+})
+
+client.login('NzkzNDM5NTE0NDYxOTk1MDE5.X-sSIQ.S9ZKudycSwtZLkmrInzU3oHxJCQ');
